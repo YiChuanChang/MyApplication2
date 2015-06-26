9b1b7d4e3abf202085947173fc059f697b359d71
Tony <yichiuanjuan1995@yahoo.com.tw>
2015-06-26 18:37:50 +0800

    correctly add new cylinder

--- app/src/main/java/netdb/course/softwarestudion/myapplication/DrawCylinder.java
+++ app/src/main/java/netdb/course/softwarestudion/myapplication/DrawCylinder.java
@@ -260,11 +260,11 @@ public class DrawCylinder
 
         gl.glBindTexture(GL10.GL_TEXTURE_2D, textureId);
 
-        gl.glDrawArrays(GL10.GL_TRIANGLES, 0, vCount);//繪製圖像
+        gl.glDrawArrays(GL10.GL_TRIANGLES, 0, vCount);//繪製圖像面
 
-        gl.glColor4f(1f, 1f, 1f, 1f);//繪製線的顏色
+        gl.glColor4f(1f, 0f, 0f, 1f);//繪製線的顏色
 
-        gl.glDrawArrays(GL10.GL_LINES, 0, vCount);//繪製圖像
+        gl.glDrawArrays(GL10.GL_LINES, 0, vCount);//繪製圖像線
 
         gl.glDisableClientState(GL10.GL_TEXTURE_COORD_ARRAY);//關閉緩衝
 
